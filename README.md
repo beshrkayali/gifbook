af44e8ce1b45031e10cf607c137682b6c4070567
Beshr Kayali <beshrkayali@gmail.com>
2015-08-16 12:50:32 +0200

    Set position for subtitles and updated READMe

--- README.md
+++ README.md
@@ -2,6 +2,38 @@
 Create sequenced Gifs from video clips and subtitles.
 
 
+## Example
+
+```python
+from gb import GifBook
+
+gb = GifBook('Night.Of.The.Living.Dead.1968.720p.mkv', '~/Desktop/gifs/')
+
+gb.set_subtitles(
+    "Night.Of.The.Living.Dead.1968.720p.srt",
+
+    subtitles_config={
+        'font_size': 26,
+        'font_name': 'Helvetica',
+        'stroke_width': 2.1,
+        'stroke_color': 'grey',
+        'position_y': 300
+    }
+)
+
+gb.generate(resize=0.6)
+```
+
+## Prerequisites
+
+Make sure that you have ImageMagick and GhostScript installed:
+
+```bash
+$ brew install ImageMagick
+$ brew install gs
+```
+
+
 ## License
 License under [GNU GPL](http://www.gnu.org/licenses/gpl.txt)
 
--- gb.py
+++ gb.py
@@ -22,10 +22,10 @@ class GifBook(object):
         self._clip = VideoFileClip(clip)
         self._output_dir = output_dir
 
-    def generate(self, start_sub, end_sub, resize=.5, compression=20):
+    def generate(self, start_sub=None, end_sub=None, resize=.5, compression=20):
         subs = pysrt.open(self._subtitles_file)
 
-        for iter_, sub in enumerate(subs):
+        for iter_, sub in enumerate(subs, start_sub if start_sub else 0):
             if end_sub and iter_ > end_sub:
                 break
 
@@ -44,10 +44,8 @@ class GifBook(object):
 
             compositions = [clip]
 
-            base_y = 200
-
             for line in sub.text.split('\n'):
-                base_y += 20
+                self._subtitles_position_y += 20
 
                 text = (
                     TextClip(line,
@@ -56,7 +54,10 @@ class GifBook(object):
                              stroke_width=self.subtitles_stroke_width,
                              stroke_color=self.subtitles_stroke_color,
                              font=self.subtitles_font_name)
-                    .set_pos((20, base_y))
+
+                    .set_pos((self._subtitles_position_x,
+                              self._subtitles_position_y))
+
                     .set_duration(clip.duration))
 
                 compositions.append(text)
@@ -65,7 +66,7 @@ class GifBook(object):
 
             composition.write_gif(gif_file_name, fuzz=compression)
 
-    def set_subtitles(self, subtitles_file, subtitles_config):
+    def set_subtitles(self, subtitles_file, subtitles_config=None):
         self._subtitles_file = subtitles_file
 
         if subtitles_config is None:
@@ -87,6 +88,12 @@ class GifBook(object):
         self._subtitles_stroke_color = subtitles_config.get(
             'stroke_color', 'black')
 
+        self._subtitles_position_x = subtitles_config.get(
+            'position_x', 20)
+
+        self._subtitles_position_y = subtitles_config.get(
+            'position_y', 200)
+
     @property
     def subtitles_font_size(self):
         return self._subtitles_font_size
@@ -106,3 +113,11 @@ class GifBook(object):
     @property
     def subtitles_stroke_color(self):
         return self._subtitles_stroke_color
+
+    @property
+    def subtitles_position_x(self):
+        return self._subtitles_position_x
+
+    @property
+    def subtitles_position_y(self):
+        return self._subtitles_position_y
